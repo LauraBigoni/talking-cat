34dc59fd3642477e0134666c3e5d611597e931f5
Laura Bigoni <laurabigoni.92@gmail.com>
2022-06-13 14:26:45 +0200

    duck is now a cat

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-console.log('JS OK!');
+// console.log('JS OK!');
 
 // * Elementi dal DOM
 const textArea = document.querySelector('textarea');
 const playButton = document.querySelector('button');
 const pitchBar = document.querySelector('input');
-const duckFigure = document.querySelector('figure');
+const catFigure = document.querySelector('figure');
 
 // * Prendo le voci del sintetizzatore vocale
 let voices = [];
 speechSynthesis.addEventListener('voiceschanged', function () {
   voices = speechSynthesis.getVoices();
-  console.log(voices);
+  // console.log(voices);
 });
 
 // * Aggiungo un Event Listener al button
@@ -21,18 +21,18 @@ playButton.addEventListener('click', function () {
 
   // * Se non ci sono
   if (textLength > 0) {
-    // * La paperella parla
+    // * Il gattino parla
     talk();
   }
 });
 
-// * Funzione per far parlare la paperella
+// * Funzione per far parlare il gattino
 function talk() {
 
   // * Tono di voce e testo
   const text = textArea.value;
   const pitch = pitchBar.value;
-  console.log(text, pitch);
+  // console.log(text, pitch);
   // * Preparo il sintetizzatore vocale e gli passo la frase dalla textarea
   const utterance = new SpeechSynthesisUtterance(text);
 
@@ -47,35 +47,33 @@ function talk() {
       return true;
     }
   });
-  console.log(voiceChoosen);
+  // console.log(voiceChoosen);
 
   // * Imposto la voce del sintetizzatore appena scelta
   utterance.voice = voiceChoosen;
 
-  // * La paperella parla
+  // * Il gattino parla
   speechSynthesis.speak(utterance);
 
-  // * Disabilito input textarea e button mentre la paperella parla
+  // * Disabilito input textarea e button mentre il gattino parla
   utterance.addEventListener('start', function () {
     // *Blocco i controlli
     textArea.disabled = true;
     pitchBar.disabled = true;
     playButton.disabled = true;
-    console.log(textArea.disabled);
-    // * Imposto la paperella animata sullo schermo
-    duckFigure.classList.add('talking');
 
+    // * Imposto il gattino animata sullo schermo
+    catFigure.classList.add('talking');
   });
 
-  // * Abilito input textarea e button quando la paperella ha finito
+  // * Abilito input textarea e button quando il gattino ha finito
   utterance.addEventListener('end', function () {
     // * Sblocco i controlli
     textArea.disabled = false;
     pitchBar.disabled = false;
     playButton.disabled = false;
-    console.log(textArea.disabled);
-    // * Reimposto la paperella statica sullo schermo
-    duckFigure.classList.remove('talking');
 
+    // * Reimposto il gattino statico sullo schermo
+    catFigure.classList.remove('talking');
   });
 }
